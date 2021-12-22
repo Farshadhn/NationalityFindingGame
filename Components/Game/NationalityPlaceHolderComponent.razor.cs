a8be1aeb1f2cb874ee2160fea5c3786eeeb1fc3b
Farshad HN <sfarshadhn@outlook.com>
2021-12-22 16:25:39 +0330

    Add project files.

---
+++ Components/Game/NationalityPlaceHolderComponent.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+
+namespace NationalityFindingGame.Components.Game;
+
+public partial class NationalityPlaceHolderComponent
+{
+
+    #region ... Parameters ...
+    [Parameter]
+    public string Content { get; set; }
+
+    [Parameter]
+    public Point Position { get; set; }
+     
+
+    #endregion
+
+
+
+}
+
---
+++ Models/Game/NationalityPlaceHolderModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace NationalityFindingGame.Models.Game;
+
+public struct NationalityPlaceHolderModel
+{
+    public Point Position { get; set; } = new Point(0, 0); 
+    public string Content { get; set; } = "Content";
+
+    public NationalityPlaceHolderModel(string content)  
+    {
+        Content = content;
+    }
+
+    public NationalityPlaceHolderModel(string content, Point position)  
+    {
+        Content = content;
+        Position = position;
+    }
+}
+
